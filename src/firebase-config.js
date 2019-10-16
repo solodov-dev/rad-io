f67a23743e83799c951f87aa244f3e5db6771701
Solodov <solodov-dev@gmail.com>
2019-10-16 18:23:09 +0300

    Firebase confing

---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyC45CK5kgJAu3DHMXpgYayoz4_07kMxJVY',
+  authDomain: 'rad-io-7c5cb.firebaseapp.com',
+  databaseURL: 'https://rad-io-7c5cb.firebaseio.com',
+  projectId: 'rad-io-7c5cb',
+  storageBucket: 'rad-io-7c5cb.appspot.com',
+  messagingSenderId: '473844176516',
+  appId: '1:473844176516:web:81700cf1fe3349e4fc94d1',
+};
+
+export default firebaseConfig;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
+import firebase from 'firebase';
+import firebaseConfig from './firebase-config';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export const eventBus = new Vue();
 
